d2808ed6c3537c3ccef3c5f048fbfe4161f019bd
Angelo Milien <angelomilienjoseph@gmail.com>
2022-01-21 03:54:47 -0500

    commit tree e8d15217d06c6bd7c7fb86745c8b217f8c51ba85

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const testVar = {}
 
 function testFunc() {
-  return "hi"
+  // return "hi"
+  console.log("jskdjsdk")
+}
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return result ? result.year : result
 }
